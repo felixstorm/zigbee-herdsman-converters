29ac6f15a0e4412c89e68cd16c29a7bb009dc66f
Felix Storm <felix.storm@glueckkanja.com>
2020-03-22 07:14:29 +0100

    switch manufacturer from Gira to Insta/Gira/Jung for '2430-100'

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -2936,7 +2936,7 @@ const converters = {
             return {action: `${button}_stop`};
         },
     },
-    GIRA2430_scene_click: {
+    insta_scene_click: {
         cluster: 'genScenes',
         type: 'commandRecall',
         convert: (model, msg, publish, options, meta) => {
@@ -2945,21 +2945,7 @@ const converters = {
             };
         },
     },
-    GIRA2430_on_click: {
-        cluster: 'genOnOff',
-        type: 'commandOn',
-        convert: (model, msg, publish, options, meta) => {
-            return {action: 'on'};
-        },
-    },
-    GIRA2430_off_click: {
-        cluster: 'genOnOff',
-        type: 'commandOffWithEffect',
-        convert: (model, msg, publish, options, meta) => {
-            return {action: 'off'};
-        },
-    },
-    GIRA2430_down_hold: {
+    insta_down_hold: {
         cluster: 'genLevelCtrl',
         type: 'commandStep',
         convert: (model, msg, publish, options, meta) => {
@@ -2971,7 +2957,7 @@ const converters = {
             };
         },
     },
-    GIRA2430_up_hold: {
+    insta_up_hold: {
         cluster: 'genLevelCtrl',
         type: 'commandStepWithOnOff',
         convert: (model, msg, publish, options, meta) => {
@@ -2983,7 +2969,7 @@ const converters = {
             };
         },
     },
-    GIRA2430_stop: {
+    insta_stop: {
         cluster: 'genLevelCtrl',
         type: 'commandStop',
         convert: (model, msg, publish, options, meta) => {
